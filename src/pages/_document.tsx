bee251f16c4c46625a82320a59244cff7a702b99
danjboard2 <101198631+danjboard2@users.noreply.github.com>
2023-05-18 21:34:04 +0100

    add chatbot

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,8 @@ export default function Document() {
             <Script strategy="beforeInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/ScrollToPlugin.min.js"></Script>
             <Script strategy="lazyOnload" src="/js/script.js"></Script>
             <Script strategy="lazyOnload" src="/js/scroll-script.js"></Script>
-            <Script src="https://player.vimeo.com/api/player.js"></Script>
+            <Script strategy="lazyOnload" src="https://player.vimeo.com/api/player.js"></Script>
+            <Script strategy="lazyOnload" src="//code.tidio.co/zjoi0ajovrui5txvitkzweydom4tlltp.js"></Script>
       </Head>
       <body>
         <Main />
