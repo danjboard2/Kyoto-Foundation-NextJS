133ea247df6aaacb333966bbd6706d9d37b86492
danjboard2 <101198631+danjboard2@users.noreply.github.com>
2023-05-21 23:08:59 +0100

    scripts not loading properly

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,12 +9,12 @@ export default function Document() {
       <Script strategy="beforeInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/gsap.min.js"></Script>
             <Script strategy="afterInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/ScrollTrigger.min.js"></Script>
             <Script strategy="afterInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/ScrollToPlugin.min.js"></Script>
-            <Script strategy="lazyOnload" src="/js/script.js"></Script>
-            <Script strategy="lazyOnload" src="/js/scroll-script.js"></Script>
-            <Script strategy="lazyOnload" src="/js/basicLightbox.min.js"></Script>
-            <Script strategy="lazyOnload" src="https://player.vimeo.com/api/player.js"></Script>
-            <Script strategy="lazyOnload" src="//code.tidio.co/zjoi0ajovrui5txvitkzweydom4tlltp.js"></Script>
-            <Script strategy="lazyOnload" src="/js/comp-script.js"></Script>
+            <Script strategy="afterInteractive" src="/js/script.js"></Script>
+            <Script strategy="beforeInteractive" src="/js/scroll-script.js"></Script>
+            <Script strategy="beforeInteractive" src="/js/basicLightbox.min.js"></Script>
+            <Script strategy="afterInteractive" src="https://player.vimeo.com/api/player.js"></Script>
+            <Script strategy="afterInteractive" src="//code.tidio.co/zjoi0ajovrui5txvitkzweydom4tlltp.js"></Script>
+            <Script strategy="beforeInteractive" src="/js/comp-script.js"></Script>
       </Head>
       <body>
         <Main />
