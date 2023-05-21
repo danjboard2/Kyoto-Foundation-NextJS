240d4d65e452717cecaa43f0d9b40046af1496d3
danjboard2 <101198631+danjboard2@users.noreply.github.com>
2023-05-21 23:32:10 +0100

    fix?

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,8 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-            <Script strategy="beforeInteractive" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></Script>
-            <Script strategy="beforeInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/gsap.min.js"></Script>
-            <Script strategy="beforeInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/ScrollTrigger.min.js"></Script>
-            <Script strategy="beforeInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/ScrollToPlugin.min.js"></Script>
-            <Script strategy="afterInteractive" src="/js/script.js"></Script>
-            <Script strategy="beforeInteractive" src="/js/scroll-script.js"></Script>
-            <Script strategy="afterInteractive" src="/js/basicLightbox.min.js"></Script>
-            <Script strategy="afterInteractive" src="https://player.vimeo.com/api/player.js"></Script>
-            <Script strategy="afterInteractive" src="//code.tidio.co/zjoi0ajovrui5txvitkzweydom4tlltp.js"></Script>
-            <Script strategy="afterInteractive" src="/js/comp-script.js"></Script>
       <Head>
       </Head>
       <body>
