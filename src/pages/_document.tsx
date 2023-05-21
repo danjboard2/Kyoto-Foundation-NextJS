d910683601f60d2466e079fe167c75ab4bcb0aa0
danjboard2 <101198631+danjboard2@users.noreply.github.com>
2023-05-21 23:23:02 +0100

    fix??

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,8 @@ import Script from 'next/script'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-      <Script strategy="beforeInteractive" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></Script>
-      <Script strategy="beforeInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/gsap.min.js"></Script>
+            <Script strategy="beforeInteractive" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></Script>
+            <Script strategy="beforeInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/gsap.min.js"></Script>
             <Script strategy="beforeInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/ScrollTrigger.min.js"></Script>
             <Script strategy="beforeInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/ScrollToPlugin.min.js"></Script>
             <Script strategy="afterInteractive" src="/js/script.js"></Script>
@@ -15,6 +14,7 @@ export default function Document() {
             <Script strategy="afterInteractive" src="https://player.vimeo.com/api/player.js"></Script>
             <Script strategy="afterInteractive" src="//code.tidio.co/zjoi0ajovrui5txvitkzweydom4tlltp.js"></Script>
             <Script strategy="afterInteractive" src="/js/comp-script.js"></Script>
+      <Head>
       </Head>
       <body>
         <Main />
