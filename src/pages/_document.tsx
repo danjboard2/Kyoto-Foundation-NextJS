93ffc77ff308c684a9d402957bfc864a36336fba
danjboard2 <101198631+danjboard2@users.noreply.github.com>
2023-05-21 23:19:35 +0100

    scripts again

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,10 +11,10 @@ export default function Document() {
             <Script strategy="beforeInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/ScrollToPlugin.min.js"></Script>
             <Script strategy="afterInteractive" src="/js/script.js"></Script>
             <Script strategy="beforeInteractive" src="/js/scroll-script.js"></Script>
-            <Script strategy="beforeInteractive" src="/js/basicLightbox.min.js"></Script>
+            <Script strategy="afterInteractive" src="/js/basicLightbox.min.js"></Script>
             <Script strategy="afterInteractive" src="https://player.vimeo.com/api/player.js"></Script>
             <Script strategy="afterInteractive" src="//code.tidio.co/zjoi0ajovrui5txvitkzweydom4tlltp.js"></Script>
-            <Script strategy="beforeInteractive" src="/js/comp-script.js"></Script>
+            <Script strategy="afterInteractive" src="/js/comp-script.js"></Script>
       </Head>
       <body>
         <Main />
